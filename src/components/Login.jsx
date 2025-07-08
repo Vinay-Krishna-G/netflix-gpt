d8e1ca940b37bbfbc3a81b6471b0fb27374239fa
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-07-08 18:48:41 +0530

    vadilating data and firebase init

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import Header from "./Header";
+import { checkValidData } from "../utils/validate";
 
 const Login = () => {
   const [isSignIn, setSignIn] = React.useState(true);
+  const [errorMessage, setErrorMessage] = React.useState(null);
+
+  const email = React.useRef(null);
+  const password = React.useRef(null);
+  const firstName = React.useRef(null);
+  const lastName = React.useRef(null);
+
+  const handleButtonClick = () => {
+    let message;
+    if (isSignIn) {
+      message = checkValidData(email.current.value, password.current.value);
+    } else {
+      message = checkValidData(
+        email.current.value,
+        password.current.value,
+        firstName.current.value,
+        lastName.current.value
+      );
+    }
+    setErrorMessage(message);
+  };
 
   const toggleForm = () => {
     setSignIn(!isSignIn);
@@ -16,7 +38,10 @@ const Login = () => {
         <img src="https://assets.nflxext.com/ffe/siteui/vlv3/05e91faa-6f6d-4325-934e-5418dcc2567b/web/IN-en-20250630-TRIFECTA-perspective_159086b1-425f-435b-bcd5-1ed8039cdef9_large.jpg" />
       </div>
 
-      <form className="w-3/12 absolute bg-black p-12 mx-auto my-38 right-0 left-0 text-white opacity-85">
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute bg-black p-12 mx-auto my-38 right-0 left-0 text-white opacity-85"
+      >
         <h1 className="text-3xl font-bold py-4">
           {isSignIn ? "Sign In" : "Sign Up"}
         </h1>
@@ -24,11 +49,13 @@ const Login = () => {
         {!isSignIn && (
           <div>
             <input
+              ref={firstName}
               type="text"
               placeholder="Fisrt Name"
               className="p-4 my-4 border border-gray-300 rounded w-full bg-gray-600"
             />
             <input
+              ref={lastName}
               type="text"
               placeholder="Last Name"
               className="p-4 my-4 border border-gray-300 rounded w-full bg-gray-600"
@@ -37,16 +64,23 @@ const Login = () => {
         )}
 
         <input
+          ref={email}
           type="email"
           placeholder="Email"
           className="p-4 my-4 border border-gray-300 rounded w-full bg-gray-600"
         />
         <input
+          ref={password}
           type="password"
           placeholder="Password"
           className="p-4 my-4 border border-gray-300 rounded w-full bg-gray-600"
         />
-        <button className="p-4 my-6 bg-red-600 text-white hover:bg-red-700 w-full rounded-lg">
+
+        <p className="text-red-400 py-2 font-bold text-l">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-600 text-white hover:bg-red-700 w-full rounded-lg cursor-pointer"
+          onClick={handleButtonClick}
+        >
           {isSignIn ? "Sign In" : "Sign Up"}
         </button>
         <p className="py-4" onClick={toggleForm}>
