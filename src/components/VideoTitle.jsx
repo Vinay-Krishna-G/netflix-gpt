f35c0543f703329e1ff3e28978a9f0155b13d7fa
Vinay-Krishna-G <vinaykrishna1515@gmail.com>
2025-07-13 12:28:13 +0530

    autoplay bg video

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -6,10 +6,10 @@ const VideoTitle = ({ title, overview }) => {
       <h1 className="text-5xl font-bold">{title}</h1>
       <p className=" py-5 text-md w-1/3">{overview}</p>
       <div>
-        <button className="cursor-pointer bg-red-500 text-white font-bold w-30 h-12 rounded-md m-2 p-2">
+        <button className="cursor-pointer bg-red-500 text-white text-xl font-bold w-30 h-12 rounded-md m-2 p-2 hover:bg-red-400">
           Play{" "}
         </button>
-        <button className="cursor-pointer bg-orange-50 text-red-500 font-bold w-30 h-12 rounded-md m-2 p-2 pb-5">
+        <button className="cursor-pointer bg-orange-50 text-red-500 text-md font-bold w-30 h-12 rounded-md m-2 p-2 pb-5 hover:bg-orange-100">
           {" "}
           <span className="text-xl">🛈</span> More info{" "}
         </button>
